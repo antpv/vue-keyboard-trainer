7bb535fa387821fc756969225fa432cee2cf7ef6
Dmitry Antipov <antpv99@gmail.com>
2021-03-04 23:34:06 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="root">
+    <FontsLoader @load="fontsLoaded = true" />
+
+    <template v-if="fontsLoaded">
+      App
+    </template>
+  </div>
+</template>
+
+<script>
+import FontsLoader from './components/FontsLoader.vue';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      fontsLoaded: false,
+    };
+  },
+
+  components: {
+    FontsLoader,
+  },
+};
+</script>
+
+<style lang="scss">
+.root {
+  font-family: 'Inter', sans-serif;
+}
+</style>
---
+++ src/components/FontsLoader.vue
@@ -0,0 +1,41 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FontsLoader',
+
+  created() {
+    const { head } = document;
+
+    this.loadLinkPreconnect(head);
+    this.loadStylesSheet(head);
+
+    window.onload = () => {
+      this.$emit('load');
+    };
+  },
+
+  methods: {
+    loadLinkPreconnect(parentElement) {
+      const linkPreconnect = document.createElement('link');
+
+      linkPreconnect.setAttribute('rel', 'preconnect');
+
+      parentElement.appendChild(linkPreconnect);
+    },
+
+    loadStylesSheet(parentElement) {
+      const linkStylesSheet = document.createElement('link');
+
+      linkStylesSheet.setAttribute('rel', 'stylesheet');
+      linkStylesSheet.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap');
+
+      parentElement.appendChild(linkStylesSheet);
+    },
+  },
+
+  render() {
+    return null;
+  },
+});
+</script>
