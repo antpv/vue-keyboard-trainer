35375659e6ec89337bae39b0151498eac1eb02f4
Dmitry Antipov <antpv99@gmail.com>
2021-03-04 23:53:46 +0200

    Add keyboard map

---
+++ src/components/KeyboardEmitter.vue
@@ -0,0 +1,20 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'KeyboardEmitter',
+
+  created() {
+    document.addEventListener('keydown', (event) => {
+      console.log(event);
+    });
+  },
+
+  methods: {
+  },
+
+  render() {
+    return null;
+  },
+});
+</script>
