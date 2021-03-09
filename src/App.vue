634ee4e85668929832465da8060e784594b7237d
Dmitry Antipov <antpv99@gmail.com>
2021-03-10 00:25:16 +0200

    App preloader implementation

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,72 @@
 <template>
-  <div class="root">
-    <FontsLoader @load="fontsLoaded = true" />
-
-    <template v-if="fontsLoaded">
-      App
-    </template>
+  <div v-if="appPrefetching" class="hero-loader">
+    <div class="hero-loader__spinner" />
+  </div>
+  <div v-else>
+    App
   </div>
 </template>
 
 <script>
-import FontsLoader from './components/FontsLoader.vue';
-
 export default {
   name: 'App',
 
   data() {
     return {
-      fontsLoaded: false,
+      appPrefetching: true,
     };
   },
 
-  components: {
-    FontsLoader,
+  created() {
+    this.initApp();
+  },
+
+  methods: {
+    initApp() {
+      const minPreload = 500;
+      const startTime = performance.now();
+
+      window.onload = () => {
+        const endTime = performance.now();
+        const isBeforeTheMinimumPreload = endTime - startTime < minPreload;
+        const leftTime = minPreload - endTime - startTime;
+
+        if (isBeforeTheMinimumPreload) {
+          setTimeout(() => {
+            this.appPrefetching = false;
+          }, leftTime);
+        } else {
+          this.appPrefetching = false;
+        }
+      };
+    },
   },
 };
 </script>
 
 <style lang="scss">
-.root {
-  font-family: 'Inter', sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Recursive:wght@300;400;500&display=swap');
+
+body {
+  font-family: 'Recursive', sans-serif;
+  background-color: #ffe8d6;
+  margin: 0;
+}
+
+.hero-loader {
+  width: 100wh;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__spinner {
+    width: 24px;
+    height: 24px;
+    border: 2px solid #cb997e;
+    border-radius: 12px;
+    display: inline-block;
+  }
 }
 </style>
